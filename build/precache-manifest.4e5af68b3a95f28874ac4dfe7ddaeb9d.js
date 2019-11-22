a0b26c779943be95ca3acccbebb039203e061399
rajju <vajrachara@yahoo.com>
2019-11-22 09:30:44 +0000

    build folder allowed to git

---
+++ build/precache-manifest.4e5af68b3a95f28874ac4dfe7ddaeb9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c17ea05090b1c6ca39f5cf1d32b245d",
+    "url": "/react-base-theme/build/index.html"
+  },
+  {
+    "revision": "a6c5ee7952f387611233",
+    "url": "/react-base-theme/build/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f600cad6f8dea5d8cade",
+    "url": "/react-base-theme/build/static/js/2.ca003cc0.chunk.js"
+  },
+  {
+    "revision": "a6c5ee7952f387611233",
+    "url": "/react-base-theme/build/static/js/main.9e30f035.chunk.js"
+  },
+  {
+    "revision": "3678077d18e7b19e990a",
+    "url": "/react-base-theme/build/static/js/runtime-main.3d07bb0e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-base-theme/build/static/media/logo.25bf045c.svg"
+  }
+]);

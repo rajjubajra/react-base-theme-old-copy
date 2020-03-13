9b385c071316d626e94f04d2d3b4d5ff1cf3ad68
rajju <vajrachara@yahoo.com>
2020-03-13 16:46:52 +0000

    .env added

--- src/components/Websites/SearchRecipes/action/actionSearch.js
+++ src/components/Websites/SearchRecipes/action/actionSearch.js
@@ -5,18 +5,23 @@
 - https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=API-KEY
 - Now you can use endpoints following our documentation.
 - Download an SDK to embed the API into your project or access the API via our Postman collection.
+
+Example Request and Response
+GET https://api.spoonacular.com/recipes/search?query=cheese&number=2
  * 
  */
 
 
 import { actionTypes } from './actionTypes';
 import axios from 'axios';
+require('dotenv').config();
 
 
 export function actionSearch(text) {
 
 
-  const url = "https://jsonplaceholder.typicode.com/posts/";
+  //const url = "https://jsonplaceholder.typicode.com/posts/";
+  const url = "https://api.spoonacular.com/recipes/search?query=cheese&number=2&apiKey=" + process.env.SPOONACULAR_API;
 
 
   return function (dispatch) {
